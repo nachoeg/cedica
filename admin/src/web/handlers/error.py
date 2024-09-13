9b4fba56882bc44317ca27d1b0fd3f22b9734556
Verónica Cao <cao.veronica@gmail.com>
2024-09-13 03:51:33 -0300

    Agrego docstring al manejador de error 404

--- admin/src/web/handlers/error.py
+++ admin/src/web/handlers/error.py
@@ -8,6 +8,11 @@ class Error:
     description: str
 
 def error_not_found(e):
+    """Retorna el template de error cuando no se encontró la página solicitada.
+    
+    Argumentos:
+    e -- la excepción
+    """
     error = Error(404, "Not found", "The requested URL was not found on the server.")
 
     return render_template("error.html", error=error), error.code
