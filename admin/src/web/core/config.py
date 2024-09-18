0af79eb890897cff48f9dd8b4b7fa3a5ddd9a077
Matias Geringer <matiasgeringer7@gmail.com>
2024-09-18 19:53:25 -0300

    Subo archivo config

---
+++ admin/src/web/core/config.py
@@ -0,0 +1,26 @@
+from os import environ
+
+class Config(object):
+	TESTING = False
+	
+class ProductionConfig(Config):
+	SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+	
+class DevelopmentConfig(Config):
+	DB_USER = "postgres"
+	DB_PASSWORD = "postgres"
+	DB_HOST = "localhost"
+	DB_PORT = "5432"
+	DB_NAME = "grupo17"
+	SQLALCHEMY_DATABASE_URI = (
+	f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+	)
+
+class TestingConfig(Config):
+	TESTING = True
+
+config = {
+	"development": DevelopmentConfig,
+	"production": ProductionConfig,
+	"testing": TestingConfig,
+}
