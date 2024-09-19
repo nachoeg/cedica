3a2416d0c6957b4e72d72b5b8a8a829c46e2dfcf
Ignacio Escudero Gonzalez <nachoeg99@gmail.com>
2024-09-19 15:31:18 -0300

    Arreglo seeds.py

--- admin/src/core/seeds.py
+++ admin/src/core/seeds.py
@@ -2,6 +2,6 @@ from src.core import auth
 
 
 def run():
-    user1 = auth.create_issue(email="juan@mail.com", alias="Juan", activo=True)
-    user2 = auth.create_issue(email="jorge@mail.com", alias="Jorge", activo=False)
-    user3 = auth.create_issue(email="maria@mail.com", alias="Maria", activo=True)
+    user1 = auth.create_user(email="juan@mail.com", alias="Juan", activo=True)
+    user2 = auth.create_user(email="jorge@mail.com", alias="Jorge", activo=False)
+    user3 = auth.create_user(email="maria@mail.com", alias="Maria", activo=True)
