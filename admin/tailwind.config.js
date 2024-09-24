d1b884e98663fd267a5d3980e18e058522802fe2
Ignacio Escudero Gonzalez <nachoeg99@gmail.com>
2024-09-24 16:54:59 -0300

    Agrego navbar, logo cedica, agrego iconos y divido en componentes

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/web/templates/*.html", "node_modules/preline/dist/*.js"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
   },
   darkMode: 'class',
   plugins: [require("preline/plugin")],
