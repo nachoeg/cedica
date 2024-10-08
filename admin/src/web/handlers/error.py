976741700f022780bb3dcd00543d5799e99d94c2
Ignacio Escudero Gonzalez <nachoeg99@gmail.com>
2024-10-08 03:06:12 -0300

    Arreglo handler de error

--- admin/src/web/handlers/error.py
+++ admin/src/web/handlers/error.py
@@ -1,18 +1,20 @@
 from flask import render_template
 from dataclasses import dataclass
 
+
 @dataclass
 class Error:
     code: int
-    message: str
+    name: str
     description: str
+
 
 def error_not_found(e):
     """Retorna el template de error cuando no se encontró la página solicitada.
-    
+
     Argumentos:
     e -- la excepción
     """
     error = Error(404, "Not found", "The requested URL was not found on the server.")
 
-    return render_template("error.html", error=error), error.code
+    return render_template("pages/error.html", error=error), error.code
