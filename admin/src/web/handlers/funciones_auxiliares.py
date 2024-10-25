7a664cef1e7517fa242c10737a8e088921cc0f25
Ignacio Escudero Gonzalez <nachoeg99@gmail.com>
2024-10-25 03:41:51 -0300

    Agrego validacion de tamaño de archivo y fecha

--- admin/src/web/handlers/funciones_auxiliares.py
+++ admin/src/web/handlers/funciones_auxiliares.py
@@ -1,3 +1,7 @@
+from wtforms.validators import ValidationError
+from datetime import date
+
+
 def booleano_a_palabra(bool):
     return ['No', 'Sí'][bool]
 
@@ -13,3 +17,25 @@ def fechahora_a_fecha(fechahora):
 
 def fechahora(fechahora):
     return fechahora.strftime('%d-%m-%Y %H:%M')
+
+
+def LimiteDeArchivo(tamanio_en_mb):
+    max_bytes = tamanio_en_mb * 1024 * 1024
+
+    def chequear_tamanio(form, field):
+        if len(field.data.read()) > max_bytes:
+            raise ValidationError(
+                f"El tamaño del archivo debe ser menor a {tamanio_en_mb}MB"
+            )
+        field.data.seek(0)
+
+    return chequear_tamanio
+
+
+def FechaNoFutura():
+
+    def chequear_fechas(form, field):
+        if field.data and field.data > date.today():
+            raise ValidationError("La fecha no puede ser posterior a hoy")
+
+    return chequear_fechas
