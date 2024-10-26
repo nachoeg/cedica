26af29492172d939ab9a7f845fce6de5f885fb4d
Lea Mankowski <lea17mk@gmail.com>
2024-10-25 21:47:53 -0300

    Se agrega validación en el tamaño de los archivos subidos por el usuario en J&A + cambio formato

--- admin/src/core/forms/forms_documentos_jya.py
+++ admin/src/core/forms/forms_documentos_jya.py
@@ -1,3 +1,4 @@
+from admin.src.core.forms.validaciones import LimiteDeArchivo
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -8,37 +9,64 @@ from wtforms.fields import (
     SubmitField,
 )
 
+
 class SubirArchivoForm(FlaskForm):
-    ''' 
-        Formulario utilizado para subir un archivo.
-    '''
+    """
+    Formulario utilizado para subir un archivo.
+    """
+
     titulo = StringField(
         "Titulo", validators=[DataRequired("Ingrese el nombre del documento")]
     )
-    archivo = FileField("Archivo", validators=[DataRequired("Seleccione un archivo")])
-    tipo_de_documento_id = SelectField("Tipo", choices=[('entrevista','Entrevista'),('evaluacion','Evaluación'),('planificacion','Planificación'),('evolucion','Evolución'),('documental','Documental')])
+    archivo = FileField(
+        "Archivo",
+        validators=[
+            DataRequired("Seleccione un archivo"),
+            LimiteDeArchivo(tamanio_en_mb=100),
+        ],
+    )
+    tipo_de_documento_id = SelectField(
+        "Tipo",
+        choices=[
+            ("entrevista", "Entrevista"),
+            ("evaluacion", "Evaluación"),
+            ("planificacion", "Planificación"),
+            ("evolucion", "Evolución"),
+            ("documental", "Documental"),
+        ],
+    )
     submit = SubmitField("Guardar")
 
 
 class EnlaceForm(FlaskForm):
-    ''' 
-        Formulario utilizado para subir un enlace.
-    '''
+    """
+    Formulario utilizado para subir un enlace.
+    """
+
     titulo = StringField(
         "Titulo", validators=[DataRequired("Ingrese el nombre del documento")]
     )
     url = StringField("Enlace", validators=[DataRequired("Ingrese el enlace")])
-    tipo_de_documento_id = SelectField("Tipo", choices=[('entrevista','Entrevista'),('evaluacion','Evaluación'),('planificacion','Planificación'),('evolucion','Evolución'),('documental','Documental')])
+    tipo_de_documento_id = SelectField(
+        "Tipo",
+        choices=[
+            ("entrevista", "Entrevista"),
+            ("evaluacion", "Evaluación"),
+            ("planificacion", "Planificación"),
+            ("evolucion", "Evolución"),
+            ("documental", "Documental"),
+        ],
+    )
     submit = SubmitField("Guardar")
 
 
 class EditarArchivoForm(FlaskForm):
-    ''' 
-        Formulario utilizado para editar un archivo.
-    '''
+    """
+    Formulario utilizado para editar un archivo.
+    """
+
     titulo = StringField(
         "Titulo", validators=[DataRequired("Ingrese el nombre del documento")]
     )
     tipo_de_documento_id = SelectField("Tipo", coerce=int)
     submit = SubmitField("Guardar")
-
--- admin/src/core/forms/forms_jinetes.py
+++ admin/src/core/forms/forms_jinetes.py
@@ -1,77 +1,150 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
-from wtforms.fields import DateTimeField, SelectField, StringField, SubmitField, IntegerField, BooleanField, TextAreaField, SelectMultipleField
+from wtforms.fields import (
+    DateTimeField,
+    SelectField,
+    StringField,
+    SubmitField,
+    IntegerField,
+    BooleanField,
+    TextAreaField,
+    SelectMultipleField,
+)
 from datetime import datetime
 
+
 class NuevoJYAForm(FlaskForm):
-    ''' 
-        Formulario utilizado para cargar la información general del jinete o amazona.
-    '''
-    nombre = StringField('Nombre', validators=[DataRequired('Ingrese el nombre del jinete o la amazona')])
-    apellido = StringField('Apellido', validators=[DataRequired('Ingrese el apellido del jinete o la amazona')])
-    dni = IntegerField('DNI')
-    edad = IntegerField('Edad')
-    fecha_nacimiento =  DateTimeField('Fecha de nacimiento', format='%d/%m/%Y')
-    provincia_nacimiento = StringField('Provincia de nacimiento', validators=[Length(max=64)])
-    localidad_nacimiento = StringField('Localidad de nacimiento', validators=[Length(max=64)])
-    domicilio_actual = StringField('Domicilio actual', validators=[Length(max=64)])
-    telefono_actual = IntegerField('Telefono actual')
-    contacto_emer_nombre = StringField('Nombre de contacto de emergencia', validators=[Length(max=64)])
-    contacto_emer_telefono = IntegerField('Telefono de contacto de emergencia')
-    becado = BooleanField('¿Tiene beca?')
-    porcentaje_beca = StringField('Porcentaje de beca', validators=[Length(max=64)])
-    submit = SubmitField('Continuar')
+    """
+    Formulario utilizado para cargar la información general del jinete o amazona.
+    """
+
+    nombre = StringField(
+        "Nombre", validators=[DataRequired("Ingrese el nombre del jinete o la amazona")]
+    )
+    apellido = StringField(
+        "Apellido",
+        validators=[DataRequired("Ingrese el apellido del jinete o la amazona")],
+    )
+    dni = IntegerField("DNI")
+    edad = IntegerField("Edad")
+    fecha_nacimiento = DateTimeField("Fecha de nacimiento", format="%d/%m/%Y")
+    provincia_nacimiento = StringField(
+        "Provincia de nacimiento", validators=[Length(max=64)]
+    )
+    localidad_nacimiento = StringField(
+        "Localidad de nacimiento", validators=[Length(max=64)]
+    )
+    domicilio_actual = StringField("Domicilio actual", validators=[Length(max=64)])
+    telefono_actual = IntegerField("Telefono actual")
+    contacto_emer_nombre = StringField(
+        "Nombre de contacto de emergencia", validators=[Length(max=64)]
+    )
+    contacto_emer_telefono = IntegerField("Telefono de contacto de emergencia")
+    becado = BooleanField("¿Tiene beca?")
+    porcentaje_beca = StringField("Porcentaje de beca", validators=[Length(max=64)])
+    submit = SubmitField("Continuar")
 
 
 class InfoSaludJYAForm(FlaskForm):
-    ''' 
-        Formulario utilizado para cargar la información de salud del jinete o amazona.
-    '''
-    certificado_discapacidad = BooleanField('¿Tiene certificado de discapacidad?')
-    diagnostico = SelectField(u'Diagnóstico', coerce=int)
-    diagnostico_otro = StringField('Otro diagnóstico', validators=[Length(max=64)])
-    tipo_discapacidad = SelectField('Tipo de discapacidad', choices=[('mental', 'Mental') , ('motora','Motora'), ('sensorial','Sensorial'), ('visceral','Visceral')])
-    submit = SubmitField('Continuar')
+    """
+    Formulario utilizado para cargar la información de salud del jinete o amazona.
+    """
+
+    certificado_discapacidad = BooleanField("¿Tiene certificado de discapacidad?")
+    diagnostico = SelectField("Diagnóstico", coerce=int)
+    diagnostico_otro = StringField("Otro diagnóstico", validators=[Length(max=64)])
+    tipo_discapacidad = SelectField(
+        "Tipo de discapacidad",
+        choices=[
+            ("mental", "Mental"),
+            ("motora", "Motora"),
+            ("sensorial", "Sensorial"),
+            ("visceral", "Visceral"),
+        ],
+    )
+    submit = SubmitField("Continuar")
 
 
 class InfoEconomicaJYAForm(FlaskForm):
-    ''' 
-        Formulario utilizado para cargar la información económica del jinete o amazona.
-    '''
-    asignacion_familiar = BooleanField('¿Recibe asignación familiar?')
-    tipo_asignacion_familiar = SelectField('Tipo de asignación familiar', choices=[('auhijo','Asignación Universal por hijo'),('auhdisc','Asignación Universal por hijo con Discapacidad'),('aaescolar','Asignación por ayuda escolar anual')])
-    beneficiario_pension = BooleanField('¿Es beneficiario de alguna pensión?')
-    tipo_pension = SelectField('Tipo de pensión', choices=[('provincial', 'Provincial'), ('nacional', 'Nacional')])
-    obra_social = StringField('Obra social', validators=[Length(max=64)])
-    num_afiliado = IntegerField('Numero de afiliado')
-    posee_curatela = BooleanField('¿Posee curatela?')
-    observaciones_obra_social = StringField('Observaciones', validators=[Length(max=64)])
-    submit = SubmitField('Continuar')
+    """
+    Formulario utilizado para cargar la información económica del jinete o amazona.
+    """
+
+    asignacion_familiar = BooleanField("¿Recibe asignación familiar?")
+    tipo_asignacion_familiar = SelectField(
+        "Tipo de asignación familiar",
+        choices=[
+            ("auhijo", "Asignación Universal por hijo"),
+            ("auhdisc", "Asignación Universal por hijo con Discapacidad"),
+            ("aaescolar", "Asignación por ayuda escolar anual"),
+        ],
+    )
+    beneficiario_pension = BooleanField("¿Es beneficiario de alguna pensión?")
+    tipo_pension = SelectField(
+        "Tipo de pensión",
+        choices=[("provincial", "Provincial"), ("nacional", "Nacional")],
+    )
+    obra_social = StringField("Obra social", validators=[Length(max=64)])
+    num_afiliado = IntegerField("Numero de afiliado")
+    posee_curatela = BooleanField("¿Posee curatela?")
+    observaciones_obra_social = StringField(
+        "Observaciones", validators=[Length(max=64)]
+    )
+    submit = SubmitField("Continuar")
 
 
 class InfoEscolaridadJYAForm(FlaskForm):
-    ''' 
-        Formulario utilizado para cargar la información relacionada a la escolaridad del jinete o amazona.
-    '''
-    nombre_escuela = StringField('Nombre de escuela', validators=[Length(max=40)])
-    direccion_escuela = StringField('Direccion de escuela', validators=[Length(max=50)])
-    telefono_escuela = IntegerField('Telefono de la escuela')
-    grado_escuela = StringField('Grado al que asiste', validators=[Length(max=4)])
-    observaciones_escuela = StringField('Observaciones', validators=[Length(max=100)])
-    profesionales_a_cargo = TextAreaField('Profesionales a cargo',validators=[Length(max=200)])
-    submit = SubmitField('Continuar')
+    """
+    Formulario utilizado para cargar la información relacionada a la escolaridad del jinete o amazona.
+    """
+
+    nombre_escuela = StringField("Nombre de escuela", validators=[Length(max=40)])
+    direccion_escuela = StringField("Direccion de escuela", validators=[Length(max=50)])
+    telefono_escuela = IntegerField("Telefono de la escuela")
+    grado_escuela = StringField("Grado al que asiste", validators=[Length(max=4)])
+    observaciones_escuela = StringField("Observaciones", validators=[Length(max=100)])
+    profesionales_a_cargo = TextAreaField(
+        "Profesionales a cargo", validators=[Length(max=200)]
+    )
+    submit = SubmitField("Continuar")
+
 
 class InfoInstitucionalJYAForm(FlaskForm):
-    ''' 
-        Formulario utilizado para cargar la información institucional relacionada al jinete o amazona.
-    '''
-    propuesta_trabajo = SelectField('Propuesta de trabajo', choices=[('hipoterapia','Hipoterapia'),('monta_terapeutica','Monta terapéutica'),('deporte_ecuestre', 'Deporte ecuestre adaptado'), ('actividades_recreativas', 'Actividades recreativas'), ('equitacion', 'Equitación')])
-    condicion = SelectField('Condicion', choices=[('regular','Regular'), ('de_baja', 'De baja')])
-    sede = SelectField('Sede', choices=[('CASJ', 'CASJ'),('HLP','HLP'), ('otro', 'Otro')])
-    profesor_id = SelectField('Profesor', choices=[])
-    conductor_caballo_id = SelectField('Conductor del caballo', choices=[])
-    caballo_id = SelectField('Caballo asignado', choices=[])
-    auxiliar_pista_id = SelectField('Auxiliar de pista', choices=[])
-    dias = SelectMultipleField('Dias', choices=[('lun', 'Lunes'),('mar', 'Martes'),('mie', 'Miercoles'), ('jue', 'Jueves'),('vie', 'Viernes'),('sab', 'Sabado'), ('dom', 'Domingo') ])
-    submit = SubmitField('Finalizar carga')
+    """
+    Formulario utilizado para cargar la información institucional relacionada al jinete o amazona.
+    """
+
+    propuesta_trabajo = SelectField(
+        "Propuesta de trabajo",
+        choices=[
+            ("hipoterapia", "Hipoterapia"),
+            ("monta_terapeutica", "Monta terapéutica"),
+            ("deporte_ecuestre", "Deporte ecuestre adaptado"),
+            ("actividades_recreativas", "Actividades recreativas"),
+            ("equitacion", "Equitación"),
+        ],
+    )
+    condicion = SelectField(
+        "Condicion", choices=[("regular", "Regular"), ("de_baja", "De baja")]
+    )
+    sede = SelectField(
+        "Sede", choices=[("CASJ", "CASJ"), ("HLP", "HLP"), ("otro", "Otro")]
+    )
+    profesor_id = SelectField("Profesor", choices=[])
+    conductor_caballo_id = SelectField("Conductor del caballo", choices=[])
+    caballo_id = SelectField("Caballo asignado", choices=[])
+    auxiliar_pista_id = SelectField("Auxiliar de pista", choices=[])
+    dias = SelectMultipleField(
+        "Dias",
+        choices=[
+            ("lun", "Lunes"),
+            ("mar", "Martes"),
+            ("mie", "Miercoles"),
+            ("jue", "Jueves"),
+            ("vie", "Viernes"),
+            ("sab", "Sabado"),
+            ("dom", "Domingo"),
+        ],
+    )
+    submit = SubmitField("Finalizar carga")
