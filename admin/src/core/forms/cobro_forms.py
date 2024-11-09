63c58d647644bdb9da55d64fb7fccda68c88dcfe
Lea Mankowski <lea17mk@gmail.com>
2024-11-09 11:22:42 -0300

    Se agrega indicador de campos obligatorios a los labels

--- admin/src/core/forms/cobro_forms.py
+++ admin/src/core/forms/cobro_forms.py
@@ -8,11 +8,11 @@ class CobroForm(FlaskForm):
     ''' 
         Formulario para manejar la entidad Cobro.
     '''
-    fecha_pago = DateTimeField('Fecha de pago', format='%d/%m/%Y', validators=[DataRequired('Ingrese una fecha de pago')], default=datetime.now())
-    medio_de_pago = SelectField('Medio de pago', choices=[('efectivo', 'Efectivo'), ('credito', 'Tarjeta de crédito'), ('debito', 'Tarjeta de débito'), ('otro', 'Otro medio de pago')])
-    monto = DecimalField('Monto', validators=[DataRequired('Ingrese el monto que se cobró')])
+    fecha_pago = DateTimeField('Fecha de pago*', format='%d/%m/%Y', validators=[DataRequired('Ingrese una fecha de pago')], default=datetime.now)
+    medio_de_pago = SelectField('Medio de pago*', choices=[('efectivo', 'Efectivo'), ('credito', 'Tarjeta de crédito'), ('debito', 'Tarjeta de débito'), ('otro', 'Otro medio de pago')],render_kw={"placeholder": "Select one option"})
+    monto = DecimalField('Monto*', validators=[DataRequired('Ingrese el monto que se cobró')])
     observaciones = StringField('Observaciones', validators=[Length(max=64)])
-    joa = SelectField(u'Jinete o Amazona', coerce=int)
-    recibio_el_dinero = SelectField(u'Recibio el dinero', coerce = int)
+    joa = SelectField(u'Jinete o Amazona*', coerce=int)
+    recibio_el_dinero = SelectField(u'Recibio el dinero*', coerce = int)
     tiene_deuda = BooleanField(u'¿Tiene deuda?', default=False)
     submit = SubmitField('Aceptar')
