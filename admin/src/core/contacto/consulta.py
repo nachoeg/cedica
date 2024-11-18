b02273fe542e8228e3f9bfda1d0b0e016f5c6a65
Matias Geringer <matiasgeringer7@gmail.com>
2024-11-18 11:09:20 -0300

    Agrego el modelo de historial de estado

--- admin/src/core/contacto/consulta.py
+++ admin/src/core/contacto/consulta.py
@@ -2,33 +2,44 @@ from datetime import datetime
 from src.core.database import db
 import enum
 
+class HistorialEstado(db.Model):
+    __tablename__ = "historial_estado"
+
+
+    class Estado(enum.Enum):
+            """Clase de tipo enumerativo para gestionar las opciones de estado de las consultas"""
+
+            recibida = "Recibida"
+            leida = "Leida"
+            atendida = "Atendida"
+
+            @classmethod
+            def listar(self):
+                return self._member_map_.values()
+
+            def __str__(self):
+                return f"{self.value}"
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    estado = db.Column(db.Enum(Estado), default=Estado.recibida, nullable=False)
+    comentario = db.Column(db.Text, nullable=True)
+    usuario = db.Column(db.String(100), nullable=False)
+    activo = db.Column(db.Boolean, nullable=False, default=True)
+
 
 class Consulta(db.Model):
     """Modelo de consulta"""
 
     __tablename__ = "consultas"
 
-    class Estado(enum.Enum):
-        """Clase de tipo enumerativo para gestionar las opciones de estado de las consultas"""
-
-        recibida = "Recibida"
-        leida = "Leida"
-        atendida = "Atendida"
-
-        @classmethod
-        def listar(self):
-            return self._member_map_.values()
-
-        def __str__(self):
-            return f"{self.value}"
+    
 
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     mensaje = db.Column(db.Text, nullable=False)
     fecha = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    estado = db.Column(db.Enum(Estado), default=Estado.recibida, nullable=False)
-    comentario = db.Column(db.Text, nullable=True)
     archivado = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
