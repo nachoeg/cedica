a9cee564db6109c876b51a5e426b908aa4bd7c23
Ignacio Escudero Gonzalez <nachoeg99@gmail.com>
2024-11-18 05:23:33 -0300

    Agrego rutas (contacto y noticias) y cambios menores

--- portal/src/router/index.js
+++ portal/src/router/index.js
@@ -1,23 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import InicioView from '../views/InicioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'inicio',
+      component: InicioView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/noticias',
+      name: 'noticias',
+      component: () => import('../views/NoticiasView.vue'),
+    },
+    {
+      path: '/contacto',
+      name: 'contacto',
+      component: () => import('../views/ContactoView.vue'),
     },
   ],
+})
+
+router.afterEach((to, from, failure) => {
+  if (!failure) {
+    setTimeout(() => {
+      window.HSStaticMethods.autoInit()
+    }, 100)
+  }
 })
 
 export default router
---
+++ portal/src/views/InicioView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Inicio</h1>
+  </main>
+</template>
