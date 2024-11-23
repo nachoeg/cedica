5c5e8fac934b5e1e6f72f17b76ba55846ed9dbf3
Verónica Cao <cao.veronica@gmail.com>
2024-11-22 21:40:49 -0300

    Modifico vista inicio portal

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -6,8 +6,24 @@ import Footer from './components/Footer.vue'
 
 <template>
   <Navbar />
-  <main class="max-w-[90rem] w-full mx-auto p-2 sm:p-5 lg:px-8">
+  <main class="inicio bg-left w-full mx-auto p-2 sm:p-5 lg:px-8">
     <RouterView />
   </main>
   <Footer />
 </template>
+
+
+<style scoped> 
+.inicio { 
+  background-image: url('@/assets/background.jpg'); 
+  background-position: center bottom;
+  background-color: rgba(0, 0, 0, 0.3); 
+  background-blend-mode: overlay; 
+  background-size: cover; background-position: center; 
+  background-repeat: no-repeat;
+  height: 100vh;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  } 
+</style>
--- portal/src/components/Inicio.vue
+++ portal/src/components/Inicio.vue
@@ -3,14 +3,15 @@
 <template>
 <div class="home-container min-h-screen flex items-center justify-center"> 
   <div class="home-content text-center text-white bg-black bg-opacity-50 p-8 rounded"> 
-  
-    <h1 class="text-4xl font-bold mb-4">Somos CEDICA</h1> 
-    <p class="text-lg">Un centro dedicado a la equinoterapia que proporciona terapias
-     asistidas con caballos para mejorar la calidad de vida de nuestros usuarios.</p>
 
-    <div class="flex flex-col btn-secondary">
+    <h1 class="text-6xl font-bold mb-4">Somos CEDICA</h1> 
+    
+    <p class="text-2xl italic">Centro de Equitación para Personas con Discapacidad y Carenciadas</p>
+    <p class="text-2xl">Brindamos rehabilitación ecuestre para mejorar la calidad de vida de nuestros usuarios.</p>
+
+    <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 mt-8 rounded">
       <RouterLink to="/quienes_somos">Más información</RouterLink>
-    </div>
+    </button>
 
   </div>
 </div>
@@ -23,12 +24,3 @@
   }
 </script>
 
-<style scoped> 
-.home-container { 
-  background-image: url('@/assets/background.jpg'); 
-  background-color: rgba(0, 0, 0, 0.3); 
-  background-blend-mode: overlay; 
-  background-size: cover; background-position: center; 
-  background-repeat: no-repeat; height: 100vh;
-  } 
-</style>
