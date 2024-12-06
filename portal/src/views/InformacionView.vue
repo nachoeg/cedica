3ee501f28490ea9f87ccbc4ababe30e9c6eb4583
Verónica Cao <cao.veronica@gmail.com>
2024-12-06 04:42:44 -0300

    Información en portal: estructura inicial. Cambio en inicio

---
+++ portal/src/views/InformacionView.vue
@@ -0,0 +1,13 @@
+<template>
+  <Informacion />
+</template>
+
+<script>
+import Informacion from '../components/Informacion.vue'
+
+export default {
+  components: {
+    Informacion,
+  },
+}
+</script>
